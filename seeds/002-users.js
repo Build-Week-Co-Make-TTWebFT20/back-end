908250374a5a2cdcd0971df9d18b24b5e3c6617e
dude lao <brycelipscomb@gmail.com>
2021-02-03 14:59:10 -0500

    fixed seed data

--- seeds/002-users.js
+++ seeds/002-users.js
@@ -4,17 +4,17 @@ const rounds = process.env.BCRYPT_ROUNDS || 8;
 exports.seed = function(knex) {
   const hashedDefault = bcrypt.hashSync('password', rounds)
   return knex('users').insert([
-    {id: 1, username: 'admin', password: hashedDefault},
-    {id: 2, username: 'user', password: hashedDefault},
-    {id: 3, username: 'dummyUserForVotes1', password: hashedDefault},
-    {id: 4, username: 'dummyUserForVotes2', password: hashedDefault},
-    {id: 5, username: 'dummyUserForVotes3', password: hashedDefault},
-    {id: 6, username: 'dummyUserForVotes4', password: hashedDefault},
-    {id: 7, username: 'dummyUserForVotes5', password: hashedDefault},
-    {id: 8, username: 'dummyUserForVotes6', password: hashedDefault},
-    {id: 9, username: 'dummyUserForVotes7', password: hashedDefault},
-    {id: 10, username: 'dummyUserForVotes8', password: hashedDefault},
-    {id: 11, username: 'dummyUserForVotes9', password: hashedDefault},
-    {id: 12, username: 'dummyUserForVotes10', password: hashedDefault},
+    {username: 'admin', password: hashedDefault},
+    {username: 'user', password: hashedDefault},
+    {username: 'dummyUserForVotes1', password: hashedDefault},
+    {username: 'dummyUserForVotes2', password: hashedDefault},
+    {username: 'dummyUserForVotes3', password: hashedDefault},
+    {username: 'dummyUserForVotes4', password: hashedDefault},
+    {username: 'dummyUserForVotes5', password: hashedDefault},
+    {username: 'dummyUserForVotes6', password: hashedDefault},
+    {username: 'dummyUserForVotes7', password: hashedDefault},
+    {username: 'dummyUserForVotes8', password: hashedDefault},
+    {username: 'dummyUserForVotes9', password: hashedDefault},
+    {username: 'dummyUserForVotes10', password: hashedDefault},
   ]);
 };
