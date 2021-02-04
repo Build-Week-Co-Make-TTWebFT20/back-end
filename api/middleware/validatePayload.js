380a7c567db8b86c7e4584fdfa5e0084d0d20cb5
dude lao <brycelipscomb@gmail.com>
2021-02-03 22:51:02 -0500

    validating payloads

---
+++ api/middleware/validatePayload.js
@@ -0,0 +1,25 @@
+function validatePayload(req, res, next){
+// We're going to clean the payload to make sure it doesn't have anything
+// here we dont want.
+    let cleanPayload = {}
+    if (req.body["post_name"]){
+        cleanPayload["post_name"] = req.body["post_name"]
+    }
+    if (req.body["description"]){
+        cleanPayload["description"] = req.body["description"]
+    }
+    if (req.body["city"]){
+        cleanPayload["city"] = req.body["city"]
+    }
+    if (req.body["zip"]){
+        cleanPayload["zip"] = req.body["zip"]
+    }
+    if (req.body["abr_state"]){
+        cleanPayload["abr_state"] = req.body["abr_state"]
+    }
+    console.log(cleanPayload);
+    req.cleanPayload = cleanPayload;
+    next();
+}
+
+module.exports = validatePayload;
